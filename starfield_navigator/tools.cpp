4429eda35a3059909df56f0dc5e37dfd693d826b
Sebastian Werhausen <swerhausen@gmail.com>
2022-06-28 00:00:00 +0000

    another +3% perf

--- starfield_navigator/tools.cpp
+++ starfield_navigator/tools.cpp
@@ -7,10 +7,13 @@
 auto sfn::id::create() -> id
 {
    static id::underlying_type next_id_value = 0;
+
+#ifdef _DEBUG
    sfn_assert(
       next_id_value != std::numeric_limits<id::underlying_type>::max(),
       "id type overflowing. increase size!"
    );
+#endif
 
    id new_id{ explicit_init{}, next_id_value };
    next_id_value++;
